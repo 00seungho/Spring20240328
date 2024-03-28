eb848682e1bc5c4dd005fe06c546887c5ef6308d
00seungho <tmdgh9910@naver.com>
2024-03-28 11:53:53 +0900

    Web

---
+++ src/main/java/kr/ac/kopo/kr/spring20240328/Spring20240328Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.kr.spring20240328;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20240328Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20240328Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/kr/spring20240328/Spring20240328ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.kr.spring20240328;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring20240328ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
