27ea655d4f89ba3704b40b0080b1d6ef714435ae
00seungho <tmdgh9910@naver.com>
2024-04-18 10:44:04 +0900

    bootstrap use

--- src/main/java/kr/ac/kopo/kr/spring20240328/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/kr/spring20240328/controller/SampleController.java
@@ -50,7 +50,7 @@ public class SampleController {
     public void ex3(){
 
     }
-    @GetMapping({"/exLayout1", "/exLayout2"})
+    @GetMapping({"/exLayout1", "/exLayout2","/exTemplate","/exSidebar"})
     public void exLayout1(){
 
     }
